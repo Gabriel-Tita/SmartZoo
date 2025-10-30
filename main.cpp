c7d4b16e797705194690af7bc53ab1fc224e0f2c
Gabriel Tita <titagabriel22@gmail.com>
2025-10-30 14:31:42 +0200

    test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <string.h>
 using namespace std;
+
+class list_of_enclosures {
+private:
+    int number;
+    vector<string> name;
+
+    void update_number() {
+        number++;
+    }
+public:
+    list_of_enclosures()=default;
+
+    list_of_enclosures(const int number) {
+        this->number=number;
+    }
+
+    ~list_of_enclosures()=default;
+
+    void set_enclosure(const string& name) {
+        this->name.push_back(name);
+        update_number();
+    }
+
+    void afisare_informatii() {
+        for (int i=0;i<number;i++) {
+            cout<<i<<" "<<name[i]<<" ";
+        }
+    }
+};
 
 class enclosure
 {
@@ -14,6 +45,14 @@ public:
         this->species=species;
         this->number_of_animals=number_of_animals;
         this->number_of_enclosures=number_of_enclosures;
+    }
+
+    ~enclosure()=default;
+
+    void afisare_informatii() {
+        cout<<species<<", ";
+        cout<<number_of_animals<<", ";
+        cout<<number_of_enclosures<<"\n";
     }
 };
 
@@ -37,6 +76,8 @@ public:
         this->enclosure_number=enclosure_number;
     }
 
+    ~animal()=default;
+
     void afisare_informatii()
     {
         cout<<species_name<<", ";
@@ -55,12 +96,24 @@ class guest
 private:
     string current_position;
 public:
+
 };
 
 int main()
 {
-    animal a2;
-    animal a("lion", "great", 2, 3, 1, 1, 97, 2);
-    a.afisare_informatii();
+    list_of_enclosures list(0);
+    animal lion("lion", "great", 2, 3, 1, 1, 97, 2);
+    enclosure lion_enclosure("lion", 4, 1);
+    list.set_enclosure("lion");
+    lion.afisare_informatii();
+    lion_enclosure.afisare_informatii();
+    list.afisare_informatii();
+    //generam 100 de vizitatori
+    /**
+    vector <guest> guests[100];
+    for (int i=0;i<99;i++)
+    {
+        guests[i]=rand()%4;
+    }**/
 }
 
